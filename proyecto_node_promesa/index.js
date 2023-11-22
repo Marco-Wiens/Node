260cb67f54e048ec229c757e5e0d658f9506e9be
Marco-Wiens <marco.wiens@gmail.com>
2023-11-22 16:21:56 +0100

    primer commit

--- proyecto_node_promesa/index.js
+++
@@ -1,7 +0,0 @@
-const writeAndRead = require("./writeAndReadObject");
-const readConsole = require("./readConsole");
-
-
-// readConsole(console.log);
-
-writeAndRead("./miFichero.json", { calle: "Teruel", numero: 8 });
